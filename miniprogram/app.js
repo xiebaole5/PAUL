6c83563592ed27fb29079fcfb4c8efe3963e926a
user7374126414 <4179681593004160-user7374126414@noreply.coze.cn>
2026-01-16 00:29:30 +0800

    fix: 修改小程序API地址为远程开发环境

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -5,10 +5,10 @@ App({
     version: '1.1.2',
 
     // 后端 API 地址
-    // 开发环境: http://localhost:8000
+    // 开发环境: http://47.110.72.148
     // 生产环境: https://tnho-fasteners.com
-    // 注意：开发时使用本地地址，需要先启动后端服务: python src\main.py
-    apiBaseUrl: 'http://localhost:8000'
+    // 注意：开发时使用远程服务器地址，无需本地启动后端服务
+    apiBaseUrl: 'http://47.110.72.148'
   },
 
   onLaunch() {
