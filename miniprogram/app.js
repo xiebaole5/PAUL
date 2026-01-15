8c601857f5ed282df912128fd5b5df8ce6d1480f
user7374126414 <4179681593004160-user7374126414@noreply.coze.cn>
2026-01-16 00:43:18 +0800

    fix: 修改小程序API地址指向服务器8000端口

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -5,10 +5,10 @@ App({
     version: '1.1.2',
 
     // 后端 API 地址
-    // 开发环境: http://47.110.72.148
+    // 开发环境: http://47.110.72.148 (服务器环境)
     // 生产环境: https://tnho-fasteners.com
     // 注意：开发时使用远程服务器地址，无需本地启动后端服务
-    apiBaseUrl: 'http://47.110.72.148'
+    apiBaseUrl: 'http://47.110.72.148:8000'
   },
 
   onLaunch() {
